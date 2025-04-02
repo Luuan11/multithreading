19e26c42cac4ccfe56fca317a1058dbbb4cc0ea4
Luuan11 <luancraft55@gmail.com>
2025-04-01 23:59:53 -0300

    :tada: feat: Starting project

---
+++ cmd/api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome to the Search API! Use /{cep} to search for a CEP."))
+	})
+
+	r.HandleFunc("/{cep}", nil).Methods("GET")
+	http.ListenAndServe(":8080", r)
+}
